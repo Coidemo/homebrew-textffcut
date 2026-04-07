306aedc8eb863941e8b32c230c9774d59f11f97f
Naoki Koide <ikoan.edi5@gmail.com>
2026-04-08 08:50:31 +0900

    fix: sha256更新（GUI自動インストール対応）

--- Formula/textffcut.rb
+++ Formula/textffcut.rb
@@ -4,7 +4,7 @@ class Textffcut < Formula
   desc "Apple Silicon Mac専用バッチ文字起こしCLIツール"
   homepage "https://note.com/coidemo"
   url "https://github.com/Coidemo/TextffCut/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "84e6f79838d32fa49e4c112b9a068d8538ce7e1f4d0847c8a295b39c351f0f7b"
+  sha256 "85aeb337927cad81e7dcb69af234089ebe80ac289a688cc56224823926d31510"
   license "Proprietary"
 
   depends_on arch: :arm64
