522e95b6b313f1533ad8f78c8e060280961c31e8
Naoki Koide <ikoan.edi5@gmail.com>
2026-04-08 08:34:43 +0900

    fix: sha256更新（presentation遅延インポート対応）

--- Formula/textffcut.rb
+++ Formula/textffcut.rb
@@ -4,7 +4,7 @@ class Textffcut < Formula
   desc "Apple Silicon Mac専用バッチ文字起こしCLIツール"
   homepage "https://note.com/coidemo"
   url "https://github.com/Coidemo/TextffCut/archive/refs/tags/v2.0.1.tar.gz"
-  sha256 "c1286ed83232340d13977aa9b4187bc1e4322bcd2259f9b1059fd7b768bb0d5e"
+  sha256 "84e6f79838d32fa49e4c112b9a068d8538ce7e1f4d0847c8a295b39c351f0f7b"
   license "Proprietary"
 
   depends_on arch: :arm64
